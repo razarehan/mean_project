de01fa240f38eb1981ad4c09fff088d1424d940b
rzzza_rehan <rhn.raza121@gmail.com>
2024-07-22 23:43:44 +0530

    adding post-view component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { SignupComponent } from "./auth/signup/signup.component";
 
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
+import { PostViewComponent } from "./posts/post-view/post-view.component";
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'view/:postId', component: PostViewComponent}
 ];
 
 @NgModule({
---
+++ src/app/posts/post-view/post-view.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
+import { map } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-post-view',
+  templateUrl: './post-view.component.html',
+  styleUrls: ['./post-view.component.css']
+})
+export class PostViewComponent implements OnInit {
+  postId!: string; 
+  post!: Post;
+  
+  constructor(private route: ActivatedRoute,
+              private postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      if (window.history.state.id) {
+        this.postId = window.history.state.id;
+      }
+      this.route.params.subscribe((params: Params) => { // if user hit refresh
+        this.postId = params['postId'];
+      });
+    });
+
+    this.postsService.getPost(this.postId)
+      .pipe(map((postdata) => {
+        return {
+          id: postdata.post._id,
+          title: postdata.post.title,
+          content: postdata.post.content,
+          creator: postdata.post.creator
+        }
+      }))
+      .subscribe((transformedData) => {
+        this.post = transformedData;
+      })
+  }
+}
